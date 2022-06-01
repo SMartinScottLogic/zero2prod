6dcde3f9a6179babec6c9219e0e1f0eddbc149c9
Simon Martin <smartin@scottlogic.com>
2022-06-01 08:31:32 +0100

    Add unit tests

--- src/main.rs
+++ src/main.rs
@@ -25,3 +25,46 @@ fn health_check() -> Status {
 fn rocket() -> _ {
     rocket::build().mount("/", routes![hello, health_check])
 }
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use crate::PathBuf;
+    use rocket::http::Status;
+    use rocket::local::blocking::Client;
+
+    #[test]
+    fn hello_world() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.get(uri!(super::health_check)).dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert!(
+            response.body().is_none(),
+            "there should be no response payload"
+        );
+    }
+
+    #[test]
+    fn hello() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.get(uri!(super::hello(""))).dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert!(
+            response.body().is_some(),
+            "there should be a response payload"
+        );
+        assert_eq!(response.into_string().unwrap(), "Hello, World!");
+    }
+
+    #[test]
+    fn hello_person() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.get(uri!(super::hello("Bob"))).dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert!(
+            response.body().is_some(),
+            "there should be a response payload"
+        );
+        assert_eq!(response.into_string().unwrap(), "Hello, Bob!");
+    }
+}
